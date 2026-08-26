11a705e81b333373b1f65f0588f73e366359083c
Aaruhi <aaruhishrivastva2005@gmail.com>
2026-08-26 12:40:06 +0530

    Time: 7 ms (82.58%) | Memory: 47.8 MB (59.03%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -2,37 +2,18 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int low = 1;
         int high = 0;
-        
-        // Find the maximum pile size to set the upper bound
-        for (int pile : piles) {
-            high = Math.max(high, pile);
+        for(int pile:piles){
+            if(pile>high) high = pile;
         }
-        
-        int ans = high;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            
-            if (canEatAll(piles, h, mid)) {
-                ans = mid;        // Candidate answer found
-                high = mid - 1;   // Try searching for a smaller valid speed
-            } else {
-                low = mid + 1;    // Speed is too slow, increase it
-            }
+        while(low<=high){
+            long hour = 0;
+            int mid = low+(high-low)/2;
+            for (int pile : piles) {
+            hour += (pile + mid - 1) / mid;
+}
+            if(hour>h) low = mid+1;
+            else high = mid-1;
         }
-        
-        return ans;
-    }
-    
-    private boolean canEatAll(int[] piles, int h, int speed) {
-        long hoursNeeded = 0;
-        for (int pile : piles) {
-            // Equivalent to Math.ceil((double) pile / speed) using integer arithmetic
-            hoursNeeded += (pile + speed - 1) / speed;
-            if (hoursNeeded > h) {
-                return false; // Early exit if hours exceed limit
-            }
-        }
-        return true;
+        return low;
     }
 }
